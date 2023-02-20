f26cf5509fd1c902ed66117153e979b1cb8d0551
Marko Mandic <mandicm223@gmail.com>
2023-02-20 18:10:49 +0100

    added submit coupon functionality

--- src/app.js
+++ src/app.js
@@ -28,9 +28,13 @@ const parseCoupons = function (coupons, domain) {
         '</p></li>'
     }
 
+    if (couponHTML == '') {
+      couponHTML = '<p>Be the first to submit the coupon for this site</p>'
+    }
     var couponDisplay = document.createElement('div')
     couponDisplay.className = 'coupon_list'
     couponDisplay.innerHTML =
+      '<div class="submit_button">Submit coupon</div>' +
       '<h1>Coupons </h1> <p>Browse coupons that have been used for: <strong>' +
       domain +
       '</strong></p> <ul>' +
@@ -45,6 +49,16 @@ const parseCoupons = function (coupons, domain) {
       '<i class="fa fa-money" style="font-size:48px;"></i>'
     document.body.appendChild(couponButton)
 
+    var couponSubmitOverlay = document.createElement('div')
+    couponSubmitOverlay.className = 'submit_overlay'
+    couponSubmitOverlay.innerHTML =
+      '<h3>Do you have a coupon for this site ?</h3>' +
+      '<div><label>Code:</label><input type="text" class="code"></div>' +
+      '<div><label>Description:</label><input type="text" class="desc"></div>' +
+      '<div><button class="submit-coupon">Submit Coupon</button></div>'
+    couponSubmitOverlay.style.display = 'none'
+    document.body.append(couponSubmitOverlay)
+
     createEvents()
   } catch (e) {
     console.log('no coupons found for this domain', e)
@@ -52,6 +66,16 @@ const parseCoupons = function (coupons, domain) {
 }
 
 var createEvents = function () {
+  document
+    .querySelector('.coupon_list .submit_button')
+    .addEventListener('click', function (event) {
+      if (document.querySelector('.submit_overlay').style.display == 'block') {
+        document.querySelector('.submit_overlay').style.display = 'none'
+      } else {
+        document.querySelector('.submit_overlay').style.display = 'block'
+      }
+    })
+
   document
     .querySelector('.coupon_button')
     .addEventListener('click', function (event) {
